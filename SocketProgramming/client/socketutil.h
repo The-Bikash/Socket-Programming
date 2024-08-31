b207c4cfc989bbe490b660be588256da674796ab
Bikash Samanta <the-bikash@outlook.com>
2024-08-31 09:45:46 +0530

    Add project files.

---
+++ SocketProgramming/client/socketutil.h
@@ -0,0 +1,35 @@
+#ifndef SOCKETUTIL_SOCKETUTIL_H
+#define SOCKETUTIL_SOCKETUTIL_H
+
+#include <stdio.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <string.h>
+#include <malloc.h>
+
+
+
+struct sockaddr_in* createIPv4Address(char *ip, int port);
+
+int createTCPIpv4Socket();
+
+int createTCPIpv4Socket() { return socket(AF_INET, SOCK_STREAM, 0); }
+
+
+struct sockaddr_in* createIPv4Address(char *ip, int port) {
+
+    struct sockaddr_in  *address = malloc(sizeof(struct sockaddr_in));
+    address->sin_family = AF_INET;
+    address->sin_port = htons(port);
+
+    if(strlen(ip) ==0)
+        address->sin_addr.s_addr = INADDR_ANY;
+    else
+        inet_pton(AF_INET,ip,&address->sin_addr.s_addr);
+
+    return address;
+}
+
+
+#endif //SOCKETUTIL_SOCKETUTIL_H
